8e8e7da19ef1c56b06e0e939f5ffdd8656ae886e
mikkixx <tiktokacc095@gmail.com>
2026-05-29 00:12:49 +0300

    Изменение стилей

--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -82,13 +82,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+USE_I18N = True
+USE_L10N = True
 
 TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
